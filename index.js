ed9bbe4c3af4437b3ec3d88fab8db58220893677
apelic <apelic92@gmail.com>
2024-12-17 10:02:19 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+/*Can you live serve the HTML file?
+Is the HTML file connected to the JS file?
+Can you prompt the user for froyo flavors and store their result?
+Can you parse the user input into an array of froyo flavors?
+Can you build an object to track which flavors you have observed so far?
+As you iterate through the array of flavors, when should that object be updated?
+Is your logic organized into a function?
+
+*/
+
+
+let flavors = window.prompt("Enter your falvors below")
+
+console.log(flavors)
